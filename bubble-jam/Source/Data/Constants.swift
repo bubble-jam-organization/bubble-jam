d86d5b1812dd3814dedc15d1ca60b548beba3b69
thiagoHBA <thiagohhab@gmail.com>
2023-02-06 11:28:17 -0300

    creating folder boilerplate

---
+++ bubble-jam/Source/Data/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  bubble-jam
+//
+//  Created by Thiago Henrique on 06/02/23.
+//
+
+import Foundation
+
+struct Constants {
+    static let containerIdentifier = ""
+}
---
+++ bubble-jam/Source/Domain/DownloadAudioRoutine/DownloadAudioRoutineUseCase.swift
@@ -0,0 +1,12 @@
+//
+//  DownloadAudioRoutineUseCase.swift
+//  bubble-jam
+//
+//  Created by Thiago Henrique on 06/02/23.
+//
+
+import Foundation
+
+class DownloadAudioRoutineUseCase {
+    func execute() {}
+}
