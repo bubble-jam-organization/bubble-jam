6492b8adff585243498a00c5f8356248c3c68f18
Otávio de Albuquerque da Costa <otavio-albucosta@hotmail.com>
2023-02-13 08:57:30 -0300

    Update branch before work (#46)
    * Feat/cloudkit support (#43)
    
    * adding cloudkit support
    
    * adding a error description for database errors
    
    * creating tests for map layer
    
    * finishing repository with cloudkit communication
    
    ---------
    
    Co-authored-by: thiagoHBA <thiagohhab@gmail.com>
    
    * fixing mapper tests + comment datetime tests (#44)
    
    * fixing mapper tests + comment datetime tests
    
    * removing xcttest inside function
    
    * 41 transformar todas as strings em localized strings (#45)
    
    * locale of common text done
    
    * dynamic text size for acessibility added
    
    * constraint fixed
    
    * Update Localizable.strings
    
    * Update Localizable.strings
    
    * strange bug solved
    
    ---------
    
    Co-authored-by: NicolasBarbosa <88331625+NicolasBarbosa0@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Caio <51139215+CaioFaSoares@users.noreply.github.com>
    Co-authored-by: thiagoHBA <thiagohhab@gmail.com>
    Co-authored-by: Thiago Henrique <56696275+ThiagoHBA@users.noreply.github.com>
    Co-authored-by: NicolasBarbosa <88331625+NicolasBarbosa0@users.noreply.github.com>

---
+++ bubble-jam/Source/Data/Repository/ChallengeRepository.swift
@@ -0,0 +1,38 @@
+//
+//  ChallengeRepository.swift
+//  bubble-jam
+//
+//  Created by Thiago Henrique on 07/02/23.
+//
+
+import Foundation
+import CloudKit
+
+class ChallengeRepository: ChallengeRepositoryProtocol {
+    let database: Database
+    let mapper: any ChallengeMapperProtocol
+    
+    required init(database: Database, mapper: any ChallengeMapperProtocol) {
+        self.database = database
+        self.mapper = mapper
+    }
+    
+    func loadCurrentChallenge() async throws -> Challenge {
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: "Challenge", predicate: predicate)
+        
+        do {
+            let result = try await database.records(matching: query, inZoneWith: nil)
+            if let mostRecentChallenge = result.first {
+                let domainEntity = try await mapper.mapToDomain(mostRecentChallenge)
+                return domainEntity
+            }
+            throw RepositoryError.challengeNotFound
+        } catch {
+            if let error = error as? CKError {
+                throw DataSourceErrorHandler.handleError(error)
+            }
+            throw error
+        }
+    }
+}
