0a8958473af2ee7271f0450a6b4ac2f96b701efc
NicolasBarbosa0 <nicolasbarbosa0@gmail.com>
2023-02-15 21:49:55 -0300

    add transition without animation

--- bubble-jam/Source/Presentation/Extensions/ScrollViewExtensions.swift
+++ bubble-jam/Source/Presentation/Extensions/ScrollViewExtensions.swift
@@ -10,11 +10,20 @@ import UIKit
 extension UIScrollView {
     func scrollToBottom() {
         let bottomOffset = CGPoint(x: 0, y: contentSize.height - bounds.size.height + contentInset.bottom)
-        setContentOffset(bottomOffset, animated: true)
+        if UIAccessibility.isReduceMotionEnabled {
+            setContentOffset(bottomOffset, animated: false)
+        } else {
+            setContentOffset(bottomOffset, animated: true)
+        }
+
     }
 
     func scrollToTop() {
         let bottomOffset = CGPoint(x: 0, y: 0)
-        setContentOffset(bottomOffset, animated: true)
+        if UIAccessibility.isReduceMotionEnabled {
+            setContentOffset(bottomOffset, animated: false)
+        } else {
+            setContentOffset(bottomOffset, animated: true)
+        }
     }
 }
